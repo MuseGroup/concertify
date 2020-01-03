99abad3bdb8997829354c982e6f5e0706c0e6625
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-01-02 17:40:56 -0800

    scraper

--- src/screens/app/Home.js
+++ src/screens/app/Home.js
@@ -11,81 +11,81 @@ import {
 } from "../../shared/styledComponents";
 import Logo from "../Logo";
 
-const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: false });
+// const Nightmare = require('nightmare');
+// const nightmare = Nightmare({ show: false });
 // have this set to false for performance but can set to true if you'd like to see what it's scraping
 
-export const webScraper = () => {
-  nightmare
-    .goto(`https://www.musicfestivalwizard.com/music-festival-news`)
-    .wait('.desc_text')
-    .evaluate(() => {
-      let thing = document.querySelector('.h2');
-      return thing.innerHTML
-    })
-    .end()
-    .then((result) => {
-      return result
-    })
-    .catch((error) => {
-      console.error('Search failed:', error);
-    });
-}
-const rp = require('request-promise');
-const cheerio = require('cheerio-without-node-native');
-const Table = require('cli-table');
+// export const webScraper = () => {
+//   nightmare
+//     .goto(`https://www.musicfestivalwizard.com/music-festival-news`)
+//     .wait('.desc_text')
+//     .evaluate(() => {
+//       let thing = document.querySelector('.h2');
+//       return thing.innerHTML
+//     })
+//     .end()
+//     .then((result) => {
+//       return result
+//     })
+//     .catch((error) => {
+//       console.error('Search failed:', error);
+//     });
+// }
+// const rp = require('request-promise');
+// const cheerio = require('cheerio-without-node-native');
+// const Table = require('cli-table');
 
-let table = new Table({
-	head: ['username', '❤️', 'challenges'],
-	colWidths: [15, 5, 10]
-})
+// let table = new Table({
+// 	head: ['username', '❤️', 'challenges'],
+// 	colWidths: [15, 5, 10]
+// })
 
-const options = {
-	url: `https://forum.freecodecamp.org/directory_items?period=weekly&order=likes_received&_=1518604435748`,
-	json: true
-}
+// const options = {
+// 	url: `https://forum.freecodecamp.org/directory_items?period=weekly&order=likes_received&_=1518604435748`,
+// 	json: true
+// }
 
-rp(options)
-	.then((data) => {
-		let userData = [];
-		for (let user of data.directory_items) {
-			userData.push({name: user.user.username,likes_received: user.likes_received});
-		}
-		process.stdout.write('loading');
-		getChallengesCompletedAndPushToUserArray(userData);
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+// rp(options)
+// 	.then((data) => {
+// 		let userData = [];
+// 		for (let user of data.directory_items) {
+// 			userData.push({name: user.user.username,likes_received: user.likes_received});
+// 		}
+// 		process.stdout.write('loading');
+// 		getChallengesCompletedAndPushToUserArray(userData);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
 
-function getChallengesCompletedAndPushToUserArray(userData) {
-	var i = 0;
-	function next() {
-		if (i < userData.length) {
-			var options = {
-				url: `https://www.freecodecamp.org/` + userData[i].name,
-				transform: body => cheerio.load(body)
-			}
-			rp(options)
-				.then(function ($) {
-					process.stdout.write(`.`);
-					const fccAccount = $('h1.landing-heading').length == 0;
-					const challengesPassed = fccAccount ? $('tbody tr').length : 'unknown';
-					table.push([userData[i].name, userData[i].likes_received, challengesPassed]);
-					++i;
-					return next();
-				})
-		} else {
-			printData();
-		}
-	}
-	return next();
-};
+// function getChallengesCompletedAndPushToUserArray(userData) {
+// 	var i = 0;
+// 	function next() {
+// 		if (i < userData.length) {
+// 			var options = {
+// 				url: `https://www.freecodecamp.org/` + userData[i].name,
+// 				transform: body => cheerio.load(body)
+// 			}
+// 			rp(options)
+// 				.then(function ($) {
+// 					process.stdout.write(`.`);
+// 					const fccAccount = $('h1.landing-heading').length == 0;
+// 					const challengesPassed = fccAccount ? $('tbody tr').length : 'unknown';
+// 					table.push([userData[i].name, userData[i].likes_received, challengesPassed]);
+// 					++i;
+// 					return next();
+// 				})
+// 		} else {
+// 			printData();
+// 		}
+// 	}
+// 	return next();
+// };
 
-function printData() {
-	console.log("✅");
-	console.log(table.toString());
-}
+// function printData() {
+// 	console.log("✅");
+// 	console.log(table.toString());
+// }
 
 
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,124 +1,139 @@
-var express      = require('express');
-var path         = require('path');
-var favicon      = require('serve-favicon');
-var logger       = require('morgan');
-var bodyParser   = require('body-parser');
-var debug        = require('debug')('app:http');
-var cookieParser = require('cookie-parser');
-var passport = require('passport');
-var User = require('./models/user');
-var session = require('express-session');
-var SpotifyStrategy = require('passport-spotify').Strategy;
-var locus = require('locus');
-// Load local libraries.
-var env      = require('./config/environment'),
-    mongoose = require('./config/database'),
-    routes   = require('./config/routes');
-// Instantiate a server application.
-var app = express();
-app.use(cookieParser('notsosecretnowareyou'));
-app.use(session({
-  secret: 'programming is tough',
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+const express = require('express');
+const scraperController = require('./scraper');
+const cors = require('cors');
 
-// this comes from .env implementation
-require('dotenv').config();
+const app = express();
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-//passport stuff
-passport.use(new SpotifyStrategy({
-    clientID: process.env.CID,
-    clientSecret: process.env.CIS,
-    // callbackURL: "https://aqueous-spire-17304.herokuapp.com/callback"
-    callbackURL: "http://localhost:3000/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOne( { spotifyId: profile.id }, function (err, user) {
-      if (err) return done(err);
-      if (user) {
-        return done(null, user);
-      } else {
-        var newUser = new User({
-          spotifyId: profile.id
-        })
-        newUser.save(function(err) {
-          if (err) return done(err);
-          return done(null, newUser);
-        });
-      }
-    });
-  }
-));
-
-
-// Configure the application (and set it's title!).
-app.set('title', env.TITLE);
-app.set('safe-title', env.SAFE_TITLE);
-// EJS view engine config
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// Create local variables for use thoughout the application.
-app.locals.title = app.get('title');
-
-// Logging layer.
-app.use(logger('dev'));
-
-// Helper layer (parses the requests, and adds further data).
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-// Routing layers: favicon, static assets, dynamic routes, or 404…
-
-// Routes to static assets. Uncomment below if you have a favicon.
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Useful for debugging the state of requests.
-app.use(debugReq);
-
-// Defines all of our "dynamic" routes.
-app.use('/', routes);
-
-// Catches all 404 routes.
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// Error-handling layer.
-app.use(function(err, req, res, next) {
-  // In development, the error handler will print stacktrace.
-  err = (app.get('env') === 'development') ? err : {};
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: err
-  });
-});
-
-function debugReq(req, res, next) {
-  debug('params:', req.params);
-  debug('query:',  req.query);
-  debug('body:',   req.body);
-  next();
-}
+app.use(cors());
+app.get('/', (req, res) => res.send('hello world'));
+app.get('/festivals/*', scraperController.getData);
+app.get('/festivals', scraperController.getAllData);
+app.listen(3000);
 
 module.exports = app;
+
+
+// var express      = require('express');
+// var path         = require('path');
+// var favicon      = require('serve-favicon');
+// var logger       = require('morgan');
+// var bodyParser   = require('body-parser');
+// var debug        = require('debug')('app:http');
+// var cookieParser = require('cookie-parser');
+// var passport = require('passport');
+// var User = require('./models/user');
+// var session = require('express-session');
+// var SpotifyStrategy = require('passport-spotify').Strategy;
+// var locus = require('locus');
+// // Load local libraries.
+// var env      = require('./config/environment'),
+//     mongoose = require('./config/database'),
+//     routes   = require('./config/routes');
+// // Instantiate a server application.
+// var app = express();
+// app.use(cookieParser('notsosecretnowareyou'));
+// app.use(session({
+//   secret: 'programming is tough',
+//   resave: false,
+//   saveUninitialized: true
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// // this comes from .env implementation
+// require('dotenv').config();
+
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function(err, user) {
+//     done(err, user);
+//   });
+// });
+
+// //passport stuff
+// passport.use(new SpotifyStrategy({
+//     clientID: process.env.CID,
+//     clientSecret: process.env.CIS,
+//     // callbackURL: "https://aqueous-spire-17304.herokuapp.com/callback"
+//     callbackURL: "http://localhost:3000/callback"
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//     User.findOne( { spotifyId: profile.id }, function (err, user) {
+//       if (err) return done(err);
+//       if (user) {
+//         return done(null, user);
+//       } else {
+//         var newUser = new User({
+//           spotifyId: profile.id
+//         })
+//         newUser.save(function(err) {
+//           if (err) return done(err);
+//           return done(null, newUser);
+//         });
+//       }
+//     });
+//   }
+// ));
+
+
+// // Configure the application (and set it's title!).
+// app.set('title', env.TITLE);
+// app.set('safe-title', env.SAFE_TITLE);
+// // EJS view engine config
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// // Create local variables for use thoughout the application.
+// app.locals.title = app.get('title');
+
+// // Logging layer.
+// app.use(logger('dev'));
+
+// // Helper layer (parses the requests, and adds further data).
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// // Routing layers: favicon, static assets, dynamic routes, or 404…
+
+// // Routes to static assets. Uncomment below if you have a favicon.
+// // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Useful for debugging the state of requests.
+// app.use(debugReq);
+
+// // Defines all of our "dynamic" routes.
+// app.use('/', routes);
+
+// // Catches all 404 routes.
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // Error-handling layer.
+// app.use(function(err, req, res, next) {
+//   // In development, the error handler will print stacktrace.
+//   err = (app.get('env') === 'development') ? err : {};
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: err
+//   });
+// });
+
+// function debugReq(req, res, next) {
+//   debug('params:', req.params);
+//   debug('query:',  req.query);
+//   debug('body:',   req.body);
+//   next();
+// }
+
+// module.exports = app;
 // const createError = require('http-errors');
 // const express = require('express');
 // const path = require('path');
